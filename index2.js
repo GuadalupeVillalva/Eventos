9912c9a03b7f3f42c616d3e978953d4c2294c3be
Guadalupe Villalva <guadi_lupev@hotmail.com>
2022-09-20 23:36:44 -0300

    optimizando-proyecto-final

--- index2.js
+++ index2.js
@@ -12,29 +12,46 @@ let resultado = 0;
 //declaro un objeto
 let operacion = {
     ultimoResultado: "numero",
-    tipoDeOperacion : "dolarToARS",
+    tipoDeOperacion : "USDtoARS",
 }
 
 //declaro historial vacío
 let historial = [];
 
 //accedo al HTML
-const pesoOutput = document.getElementById("pesoArgentino");
-const dolarInput = document.getElementById("dolarInput");
-const calcularBtn = document.getElementById("calcular");
+const pesoOutput = document.getElementById('pesoArgentino');
+const monedaInput = document.getElementById('monedaInput');
+const calcularBtn = document.getElementById('calcular');
+const select = document.getElementById('select')
+
+//select value
+const convertir = () =>{
+    select.value == "usd" ? calcularUSD() : calcularEuro();
+}
 
 //declaro función para calcular
-function calcular(){
-    resultado = dolarInput.value * 276
+function calcularUSD(){
+    resultado = monedaInput.value * 287
     historial.unshift(resultado + " ")
     pesoOutput.value = resultado
     operacion.ultimoResultado = resultado
     let enJSON = JSON.stringify(operacion);
+    operacion.tipoDeOperacion = "USDtoARS"
+    localStorage.setItem("operacion", enJSON);
+}
+
+function calcularEuro(){
+    resultado = monedaInput.value * 304
+    historial.unshift(resultado + " ")
+    pesoOutput.value = resultado;
+    operacion.ultimoResultado = resultado
+    let enJSON = JSON.stringify(operacion);
+    operacion.tipoDeOperacion = "EUROtoARS"
     localStorage.setItem("operacion", enJSON);
 }
 
 //evento
-calcularBtn.addEventListener('click', calcular)
+calcularBtn.addEventListener('click', convertir)
 
 //obtengo elementos del html
 const historyContainer = document.getElementById('historyContainer') //contenedor del historial
