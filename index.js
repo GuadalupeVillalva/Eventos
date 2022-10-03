4be98a970fb0011f46eddfcd0cb1d41d9c704dcb
Guadalupe Villalva <guadi_lupev@hotmail.com>
2022-10-03 02:58:05 -0300

    fetch

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
-//función para generar un token
-function generateToken(usuario, contraseña) {
-  return usuario + contraseña;
+// fetch a API PokéApi 
+fetch('https://pokeapi.co/api/v2/pokemon?limit=50&offset=0')
+.then(res => res.json())
+.then(res => getData(res))
+
+//función para obtener los datos de la API
+function getData(dato){
+    //guardo los datos en una variable
+    const datos = dato.results
+    
+    //input en el cual se debe especificar 
+    //la cantidad de pokemones que aparecen en pantalla
+    let cantidad = document.getElementById('cantidad');
+    
+    //botón con eventListener para mostrar los nombres en pantalla
+    const confirmBtn = document.getElementById('confirm');
+
+    //evento para mostrar los nombres
+    confirmBtn.addEventListener('click', () => {
+
+        //se genera un array nuevo a partir 
+        //del número que se ingrese
+        let valorNuevo = datos.slice(0, cantidad.value)
+
+        //de ese array obtengo los nombres y los guardo
+        let names = valorNuevo.map(ob => ob.name);
+
+        //div vacío
+        let container = document.getElementById('container');
+
+        //bucle para recorrer el array y con cada 
+        //nombre crear un div con este dentro
+        names.forEach(name => {
+            //creo un div por cada nombre
+            let newData = document.createElement('div')
+
+            //dentro de este pongo una etiqueta p
+            newData.innerHTML=`
+            <p> ${name} </p>
+            `
+
+            //div vacío en el cual dentro van todos
+            //los divs creados anteriormente
+            container.appendChild(newData)
+        });
+    })
 }
 
-//función para logearse
-function login(event){
-    event.preventDefault()
-    let user = document.getElementById('user').value
-    let password = document.getElementById('password').value;
-    console.log(user)
-    console.log(password)
-    if (user !== null && password !== null) {
-    let token = generateToken(user, password);
-    localStorage.setItem("token", token);
-    }
-    Swal.fire({
-        title: "Iniciaste sesión",
-        text: "¡Ya podés usar el conversor!",
-        icon: "success",
-        confirmButtonColor: "#B1B2FF",
-        color:"#000000"
-    }
-      )
-    const theresAToken = tokenExists();
-    if(theresAToken){
-        const hiddenDiv = document.getElementById('privateContent')
-        hiddenDiv.className = "showContent"
-    }
-}
-
-//función para saber si se ha generado un token
-function tokenExists() {
-    if(localStorage.getItem('token') !== null){
-        return true;
-    } else {
-        false;
-    }
-}
-//evento botón para logearse
-button.addEventListener('click', login)
+//botón que hace refresh a la página 
+//para eliminar los elementos de la pantalla
+let reset = document.getElementById('reset')
+reset.addEventListener('click', () => {
+    window.location.reload()
+})
